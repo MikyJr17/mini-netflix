b03de4439ff7806ffa6c4d1c25564859569ddfc8
MikyJr17 <miguel00copa@gmail.com>
2025-12-26 00:53:15 -0400

    Segundo comit

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,16 +15,9 @@ import { AuthModule } from './auth/auth.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT!),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
-      ssl: {
-        rejectUnauthorized: false
-      }
     }),
     SeriesModule,
     EpisodiosModule,
