ba7f27a61948b0d312f4dcc7d4d1e2de2e1da30c
MikyJr17 <miguel00copa@gmail.com>
2025-12-26 01:13:09 -0400

    Ojala el ultimo comit

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,10 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = Number(process.env.PORT) || 3000;
+
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`🚀 App running on port ${port}`);
 }
 bootstrap();
