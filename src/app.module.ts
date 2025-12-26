8dc81be6181613106b42c38be00f95ad4a9a0d1b
MikyJr17 <miguel00copa@gmail.com>
2025-12-26 01:07:51 -0400

    Ojala el ultimo comit

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,11 @@ import { AuthModule } from './auth/auth.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
